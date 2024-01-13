8333b0db59ae8184f154f51741db15efcf03750d
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-13 09:17:27 +0100

    shuffle data with seed for each client via ip + pagination

--- src/middlewares/dataProviderMiddleware.ts
+++ src/middlewares/dataProviderMiddleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { DataProvider } from "../DataProvider";
+import { DataProvider } from "../data/DataProvider";
 
-export const dataProviderMiddleware = async (_req: Request, res: Response, next: NextFunction) => {
-    const provider = await new DataProvider('data.json').load();
+export const dataProviderMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const provider = await new DataProvider('data.json', req.ip || 'default').load();
     res.locals.provider = provider;
 
     next();
